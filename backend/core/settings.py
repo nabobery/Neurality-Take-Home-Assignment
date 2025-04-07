daffc54243bf346454d9604ba5cb424f9df84d7b
Avinash Changrani <74554800+nabobery@users.noreply.github.com>
2025-04-07 23:41:20 +0530

    Deployment of Application (#2)
    * Added required changes for sevalla deployment
    
    * minor fix in backend dockerfile
    
    * Minor: modified deploy yaml
    
    * Minor Versioning of frontend Dockerfile
    
    * Minor Fix  of frontend Dockerfile
    
    * Added separate deployment of backend using sevalla
    
    * Backend ready for deployment

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -9,10 +9,10 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'your-default-secret-key')
+# SECRET_KEY = os.environ.get('SECRET_KEY', 'your-default-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
@@ -105,7 +105,8 @@ GOOGLE_API_KEY = os.environ.get('GOOGLE_API_KEY', '')
 
 # CORS configuration - allow specific origins
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000'
+    # 'http://localhost:3000'
+    'https://rag-chatbot-zeta.vercel.app/'
 ]
 
 # CORS configuration 
